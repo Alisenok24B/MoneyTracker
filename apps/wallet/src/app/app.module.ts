7c907a7952380728d7e21a6fe5cee15ccb17739e
Alice Bikson <alisabikson@gmail.com>
2025-05-16 20:48:04 +0300

    Создан скелет для модуля кредитных карт

--- apps/wallet/src/app/app.module.ts
+++ apps/wallet/src/app/app.module.ts
@@ -7,17 +7,20 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { RMQModule } from 'nestjs-rmq';
 import { getMongoConfig } from './configs/mongo.config';
 import { getRMQConfig } from './configs/rmq.config';
+import { CreditCardModule } from './credit-card/credit-card.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({
+  imports: [
+    ConfigModule.forRoot({
       isGlobal: true,
       envFilePath: 'envs/.wallet.env',
     }),
     RMQModule.forRootAsync(getRMQConfig()),
     MongooseModule.forRootAsync(getMongoConfig()),
-    AccountModule],
+    AccountModule,
+    CreditCardModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
-
