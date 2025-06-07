3ae59dad9d63f58a8c8f92d12f5a23c7d09c4a49
Alice Bikson <alisabikson@gmail.com>
2025-06-07 23:46:54 +0300

    Получить задолженности по периодам могут и peer-ы

--- apps/api/src/app/controllers/credit-period.controller.ts
+++ apps/api/src/app/controllers/credit-period.controller.ts
@@ -7,10 +7,14 @@ import {
 } from '@moneytracker/contracts';
 import { UserId } from '../guards/user.decorator';
 import { AccountType } from '@moneytracker/interfaces';
+import { PeersHelper } from '../helpers/peer.helper';
 
 @Controller('credit-cards')
 export class CreditPeriodController {
-  constructor(private readonly rmq: RMQService) {}
+  constructor(
+    private readonly rmq: RMQService,
+    private readonly peersHelper: PeersHelper,
+  ) {}
 
   @UseGuards(JWTAuthGuard)
   @Get(':accountId/periods/debts')
@@ -18,14 +22,16 @@ export class CreditPeriodController {
     @Param('accountId') accountId: string,
     @UserId() userId: string,
   ) {
+    const peers = await this.peersHelper.getPeers(userId);
     // 1) проверяем, что счёт действительно принадлежит пользователю
     const { account } = await this.rmq.send<
       AccountGet.Request,
       AccountGet.Response
-    >(AccountGet.topic, { userId, id: accountId });
-    if (account.userId !== userId) {
-      throw new ForbiddenException('Access denied to this account');
-    }
+    >(AccountGet.topic, { userId, id: accountId, peers });
+    const allowedUsers = new Set([userId, ...peers]);
+  if (!allowedUsers.has(account.userId)) {
+    throw new ForbiddenException('Access denied to this account');
+  }
 
     // 2) проверяем, что это именно кредитная карта
     if (account.type !== AccountType.CreditCard) {
